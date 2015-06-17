9ae908e3b9a839f2a7d13d788a408d96fde1bbe7
Audun Skjelvan <audun.skjelvan@gmail.com>
2015-06-17 16:13:35 +0200

    rename

--- devtools.js
+++ devtools.js
@@ -5,4 +5,4 @@
 // Create a tab in the devtools area
 //document.getElementById("yo").innerHTML = "yoyoyo";
 //console.log("ok");
-chrome.devtools.panels.create("DemoPanel", "toast.png", "panel.html", function(panel) {});
+chrome.devtools.panels.create("Fitnesse", "toast.png", "panel.html", function(panel) {});
