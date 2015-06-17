9fd55e26147558d8cd3714d2dd8afd942d2e99c8
Audun Skjelvan <audun.skjelvan@gmail.com>
2015-06-17 14:47:10 +0200

    working example

--- inserted-script.js
+++ inserted-script.js
@@ -1,6 +1,75 @@
 // This is included and executed in the inspected page
-function inserted() {
-	console.log('External script attached');
-  console.log(document.body);
+//function inserted() {
+//  console.log('External script attached');
+//  console.log(document.body);
+//}
+//inserted();
+//
+//var id = function (el) {
+//    return document.getElementById(el);
+//};
+//
+//function print(value) {
+//    return value === '' ? '[empty]' : value;
+//}
+//
+//function log(field, value, out) {
+//  out.innerHTML += '| input | ' + field + ' | value | ' + print(value) + ' |';
+//
+//  out.innerHTML += '<br>';
+//}
+//
+//if(id('output') === null) {
+//  var elemDiv = document.createElement('div');
+//  elemDiv.setAttribute("id", "output");
+//  
+//  var infoDiv = document.createElement('div');
+//  infoDiv.setAttribute('id', 'fitnesse-form');
+//  elemDiv.appendChild(infoDiv);
+//  
+//  document.body.appendChild(elemDiv);
+//}
+//
+//id('fitnesse-form').innerHTML = 'Velg skjema';
+
+function addListener(event) {
+  console.log(event.target);
+  
+  chrome.extension.sendMessage({action: 'message', content:"Changed by page"}, function(message){});
+  
+//  document.removeEventListener('click', addListener);
+//  node = event.target;
+//  while (node.nodeName != "FORM" && node.parentNode) {
+//      node = node.parentNode;
+//  }
+//  
+//  if(node.nodeName === "FORM") {
+//    id('fitnesse-form').innerHTML = 'Skjema valgt';
+//    
+//    var w = node;
+//    var out = id('output');
+//    console.log(out);
+//
+//    function logCompleteForm() {  
+//      console.log("yoyo");
+//      console.log(out);
+//      var inputs = w.getElementsByTagName('input');
+//
+//      for(var i = 0; i<inputs.length; i++) {
+//        log(inputs[i].id, inputs[i].value, out);
+//        console.log(inputs[i].value); 
+//      }
+//    }
+//
+//    w.addEventListener('submit', function (evt) {
+//        evt.preventDefault();
+//
+//        logCompleteForm();
+//    });
+//  } else {
+//    id('fitnesse-form').innerHTML = 'Skjema ikke funnet'; 
+//  }
+  
 }
-inserted();
+
+document.addEventListener("blur", addListener, true);
