2cb77c2eedd3332678baa69a1ff0d4237fe498e2
Audun Skjelvan <audun.skjelvan@gmail.com>
2015-06-17 15:19:41 +0200

    func

--- inserted-script.js
+++ inserted-script.js
@@ -1,14 +1,22 @@
 // This is included and executed in the inspected page
 
-function sendMessage(field, value) {
-  var content = '| input | ' + field + ' | value | ' + value + ' |';
+function sendMessage(type, field, value) {
+  var content = '| ' + type + ' | ' + field + ' | value | ' + value + ' |';
   content += '<br>';
   chrome.extension.sendMessage({action: 'message', content:content}, function(message){});
 }
 
 function addListener(event) {
-  console.log(event.target);
-  sendMessage(event.target.id, event.target.value);
+  
+  if(event.target.getAttribute("type") === "submit") {
+    
+    sendMessage('submit', event.target.id, event.target.value);
+    
+  } else if(event.target.nodeName === 'INPUT') {
+    
+    sendMessage('input', event.target.id, event.target.value);
+    
+  }
   
 }
 
