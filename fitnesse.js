6833f17444b987c6c1c37dda7e58a8f2e11e3a55
Audun Skjelvan <audun.skjelvan@gmail.com>
2015-06-17 12:29:06 +0200

    wip2

--- fitnesse.js
+++ fitnesse.js
@@ -2,6 +2,20 @@ var id = function (el) {
     return document.getElementById(el);
 };
 
+function print(value) {
+    return value === '' ? '[empty]' : value;
+}
+
+function log(field, value, errorMsg) {
+    out.innerHTML += '| input | ' + field + ' | value | ' + print(value) + ' |';
+
+    if (errorMsg) {
+        out.innerHTML += ' error | ' + errorMsg + ' |';
+    }
+    
+    out.innerHTML += '<br>';
+}
+
 if(id('output') === null) {
   var elemDiv = document.createElement('div');
   elemDiv.setAttribute("id", "output");
@@ -19,10 +33,35 @@ document.addEventListener('click', function(event) {
   while (node.nodeName != "FORM" && node.parentNode) {
       node = node.parentNode;
   }
-  console.log(node);
   
   if(node.nodeName === "FORM") {
     id('fitnesse-form').innerHTML = 'Skjema valgt';
+    
+    var w = node;
+    var out = id('output');
+
+
+
+    w.addEventListener('change', function (evt) {
+        var t = evt.target;
+
+        log(t.id, t.value);
+    });
+
+    function logCompleteForm() {  
+      console.log("yoyo");
+      var inputs = w.getElementsByTagName('input');
+
+      for(var i = 0; i<inputs.length; i++) {
+        console.log(inputs[i].value); 
+      }
+    }
+
+    w.addEventListener('submit', function (evt) {
+        evt.preventDefault();
+
+        logCompleteForm();
+    });
   } else {
     id('fitnesse-form').innerHTML = 'Skjema ikke funnet'; 
   }
@@ -31,40 +70,4 @@ document.addEventListener('click', function(event) {
 });                           
   
 
-var w = id('form');
-var out = id('output');
 
-function print(value) {
-    return value === '' ? '[empty]' : value;
-}
-
-function log(field, value, errorMsg) {
-    out.innerHTML += '| input | ' + field + ' | value | ' + print(value) + ' |';
-
-    if (errorMsg) {
-        out.innerHTML += ' error | ' + errorMsg + ' |';
-    }
-    
-    out.innerHTML += '<br>';
-}
-
-w.addEventListener('change', function (evt) {
-    var t = evt.target;
-
-    log(t.id, t.value);
-});
-
-function logCompleteForm() {  
-  console.log("yoyo");
-  var inputs = w.getElementsByTagName('input');
-  
-  for(var i = 0; i<inputs.length; i++) {
-    console.log(inputs[i].value); 
-  }
-}
-
-w.addEventListener('submit', function (evt) {
-    evt.preventDefault();
-
-    logCompleteForm();
-});
